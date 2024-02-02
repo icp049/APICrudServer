e0fc8518ff0e31f695e8d80347df03ae3e71b95b
ianpe <ianpe@DESKTOP-03L8AVM>
2024-02-02 01:18:14 -0600

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APICrudServer.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace APICrudServer.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly ApiDbContext _context;
+
+        public CustomerController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Customer>>> GetCustomer()
+        {
+            return Ok(await _context.Customers.ToListAsync());
+        }
+
+    }
+}
